a5541533f2a2c425bffafa788cfc32fb4ad77ec2
BENNAI-Lyes <bennailyes19@gmail.com>
2024-05-17 09:09:49 +0100

    fix get workers route

--- routes/workerRoute.js
+++ routes/workerRoute.js
@@ -1,42 +1,42 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import { workerRegisterValidator } from "../validators/authValidator.js";
-import { runValidation } from "../validators/index.js";
-import { verifyAdmin } from "../helpers/verifyToken.js";
+import { workerRegisterValidator } from '../validators/authValidator.js';
+import { runValidation } from '../validators/index.js';
+import { verifyAdmin } from '../helpers/verifyToken.js';
 import {
-  createWorker,
-  deleteWorker,
-  getAllWorker,
-  getWorker,
-  updateWorker,
-} from "../controllers/workerController.js";
+	createWorker,
+	deleteWorker,
+	getAllWorker,
+	getWorker,
+	updateWorker,
+} from '../controllers/workerController.js';
 
 //create
 router.post(
-  "/",
-  verifyAdmin,
-  workerRegisterValidator,
-  runValidation,
-  createWorker
+	'/',
+	verifyAdmin,
+	workerRegisterValidator,
+	runValidation,
+	createWorker
 );
 
 //update
 router.put(
-  "/:id",
-  verifyAdmin,
-  workerRegisterValidator,
-  runValidation,
-  updateWorker
+	'/:id',
+	verifyAdmin,
+	workerRegisterValidator,
+	runValidation,
+	updateWorker
 );
 
 //delete
-router.delete("/:id", verifyAdmin, deleteWorker);
+router.delete('/:id', verifyAdmin, deleteWorker);
 
 //get one user
-router.get("/find/:id", verifyAdmin, getWorker);
+router.get('/find/:id', verifyAdmin, getWorker);
 
 //getAll
-router.get("/", verifyAdmin, getAllWorker);
+router.get('/', verifyAdmin, getAllWorker);
 
 //xxxxxxxx
 
